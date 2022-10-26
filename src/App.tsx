1f8ce0627e5dd3412002d88a85fa74f1863955b2
Roman Zhukov <r.zhukov@itmegabox.com>
2022-10-26 16:20:22 +0300

    check rules

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import './App.css';
 
 function App() {
-  return <div>some dawdadawdawstring</div>;
+  return <div>some dawdadawddawdawdawdawstring</div>;
 }
 
 export default App;
